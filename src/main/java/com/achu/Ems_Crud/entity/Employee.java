43344e3fa906e0113814d21458b0bc3d84bb349c
Ashwanth-R <ashwanth.achu23@gmail.com>
2024-12-25 21:31:48 +0530

    created restapi - postmapping employee details

--- src/main/java/com/achu/Ems_Crud/entity/Employee.java
+++ src/main/java/com/achu/Ems_Crud/entity/Employee.java
@@ -14,6 +14,16 @@ import lombok.Setter;
 @Table(name="employees")
 public class Employee {
 
+    public Employee(long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Employee() {
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -24,5 +34,35 @@ public class Employee {
     @Column(name="email",nullable = false, unique = true)
     private String email;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/achu/Ems_Crud/repo/EmployeeRepo.java
+++ src/main/java/com/achu/Ems_Crud/repo/EmployeeRepo.java
@@ -2,6 +2,8 @@ package com.achu.Ems_Crud.repo;
 
 import com.achu.Ems_Crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 }
