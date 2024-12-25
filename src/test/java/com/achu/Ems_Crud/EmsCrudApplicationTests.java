da537fccd92e64ae40697de0f2a29c04a86a6672
Ashwanth-R <ashwanth.achu23@gmail.com>
2024-12-25 20:05:00 +0530

    template_spring

---
+++ src/test/java/com/achu/Ems_Crud/EmsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achu.Ems_Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
